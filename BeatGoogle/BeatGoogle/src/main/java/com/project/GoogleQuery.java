b141b5d5a94521bb34a77d4119753398b68c7627
chinaoel <60130858+chinaoel@users.noreply.github.com>
2023-01-03 19:06:44 +0800

    Add files via upload
    final version

---
+++ BeatGoogle/BeatGoogle/src/main/java/com/project/GoogleQuery.java
@@ -0,0 +1,198 @@
+package com.project;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import org.json.JSONArray;
+import org.apache.commons.lang.StringEscapeUtils;
+
+public class GoogleQuery 
+{
+	public String searchKeyword;
+	public String url;
+	public String content;
+	
+	public GoogleQuery(String searchKeyword)
+	{
+		this.searchKeyword = searchKeyword;
+		this.url = "http://www.google.com/search?q="+searchKeyword+"&oe=utf8&num=20";
+	}
+	
+	public GoogleQuery(String searchKeyword, String topic) 
+	{
+		this.searchKeyword = searchKeyword;
+		this.url = String.format("http://www.google.com/search?q=%s\"%s\"&oe=utf8&num=20",searchKeyword,topic);
+	}
+	
+	private String fetchContent() throws IOException
+	{
+		String retVal = "";
+
+		URL u = new URL(url);
+		URLConnection conn = u.openConnection();
+		//set HTTP header
+		conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
+		InputStream in = conn.getInputStream();
+
+		InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+		BufferedReader bufReader = new BufferedReader(inReader);
+		String line = null;
+
+		while((line = bufReader.readLine()) != null)
+		{
+			retVal += line;
+		}
+		return retVal;
+	}
+	
+	public void FetchContentJSoup() throws IOException {
+		Document doc = Jsoup.connect(url).userAgent("Chrome/107.0.5304.107").get();
+		Elements lis = doc.select("div");
+		lis = lis.select(".kCrYT");
+		
+		for(Element li : lis)
+		{
+			try 
+			{
+				String citeUrl = li.select("a").get(0).attr("href");
+				String title = li.select("a").get(0).select(".vvjwJb").text();
+				
+				if(title.equals("")) 
+				{
+					continue;
+				}
+				
+				System.out.println("Title: " + title + " , url: " + citeUrl);
+				
+				//put title and pair into HashMap
+
+			} catch (IndexOutOfBoundsException e) 
+			{
+//				e.printStackTrace();
+			}
+		}
+	
+	}
+	
+	public ArrayList<WebTree> query() throws IOException
+	{
+		if(content == null)
+		{
+			content = fetchContent();
+		}
+		
+		ArrayList<WebTree> trees = new ArrayList<WebTree>();
+
+		HashMap<String, String> retVal = new HashMap<String, String>();
+		
+		
+		/* 
+		 * some Jsoup source
+		 * https://jsoup.org/apidocs/org/jsoup/nodes/package-summary.html
+		 * https://www.1ju.org/jsoup/jsoup-quick-start
+ 		 */
+		
+		//using Jsoup analyze html string
+		Document doc = Jsoup.parse(content);
+		
+		//select particular element(tag) which you want 
+		Elements lis = doc.select("div");
+		lis = lis.select(".kCrYT");
+		
+		for(Element li : lis)
+		{
+			try 
+			{
+				String citeUrl = li.select("a").get(0).attr("href");
+				String title = li.select("a").get(0).select(".vvjwJb").text();
+				
+				if(title.equals("")) 
+				{
+					continue;
+				}
+				
+				citeUrl = citeUrl.replace("/url?q=", "");
+				String[] arrOfStr = citeUrl.split("&sa", 2);
+				citeUrl = arrOfStr[0];
+				
+				System.out.println("Title: " + title + " , url: " + citeUrl);
+				
+				//put title and pair into HashMap
+				retVal.put(title, citeUrl);
+				WebTree root = new WebTree( new WebPage(citeUrl, title) );
+				trees.add(root);
+
+			} catch (IndexOutOfBoundsException e) 
+			{
+//				e.printStackTrace();
+			}
+		}
+		return trees;
+	}
+	
+	  public static final Map<String, String> LANGUAGE_MAP = new HashMap();
+
+	   static {
+	     init();
+	   }
+
+	   /**
+	    * 初始化语言类
+	    */
+	   private static void init() {
+	       LANGUAGE_MAP.put("auto", "Automatic");
+	       
+	   }
+	   public static String translate(String langFrom, String langTo, String word) throws Exception {
+		   
+	       String url = 
+	               "https://translate.googleapis.com/translate_a/single?" +
+	               "client=gtx" +
+	               "&sl=" + langFrom +
+	               "&tl=" + langTo +
+	               "&dt=t&q=" + URLEncoder.encode(word, "UTF-8");
+
+	       URL obj = new URL(url);
+	       HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	       con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+	       BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	       String inputLine;
+	       StringBuffer response = new StringBuffer();
+
+	       while ((inputLine = in.readLine()) != null) {
+	           response.append(inputLine);
+	       }
+	       in.close();
+
+	       return parseResult(response.toString());
+	   }
+
+	   private static String parseResult(String inputJson) throws Exception {
+
+	       JSONArray jsonArray = new JSONArray(inputJson);
+	       JSONArray jsonArray2 = (JSONArray) jsonArray.get(0);
+//	     JSONArray jsonArray3 = (JSONArray) jsonArray2.get(0);
+	       String result ="";
+
+	       for(int i =0;i < jsonArray2.length();i ++){
+	           result += ((JSONArray) jsonArray2.get(i)).get(0).toString();
+	       }
+
+	       return result;
+	   }
+}
---
+++ BeatGoogle/BeatGoogle/src/main/java/com/project/SecondLayer.java
@@ -0,0 +1,178 @@
+package com.project;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SecondLayer {
+	
+	private WebTree tree;
+	private String content;
+	private int count;
+	
+	public SecondLayer(WebTree tree) {
+		this.tree = tree;
+		this.content = "";
+		this.count = 0;
+	}
+	
+
+	
+	private String fetchContent(String url) throws IOException
+	{
+		String retVal = "";
+
+		URL u = new URL(url);
+		URLConnection conn = u.openConnection();
+		conn.setConnectTimeout(1000);
+		conn.setReadTimeout(1000);
+		//set HTTP header
+		//conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
+		InputStream in = conn.getInputStream();
+
+		InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+		BufferedReader bufReader = new BufferedReader(inReader);
+		String line = null;
+
+		while((line = bufReader.readLine()) != null)
+		{
+			retVal += line;
+		}
+		
+		this.content = retVal;
+		return retVal;
+	
+	}
+	
+
+	
+	private String URLConverter(String url) {
+		if (url.contains("https://hahow.in/courses/")) {
+			
+			
+			Pattern pattern = Pattern.compile("https://hahow.in/courses/(.*)(/*)");
+			Matcher matcher = pattern.matcher(url);
+			if (matcher.find()) {
+				String courseid = (matcher.group(1));
+				url = String.format("https://api.hahow.in/api/courses/%s", courseid) ;
+			}
+			
+			
+		}
+		
+		if (url.contains("https://hiskio.com/courses/")) {
+			
+			
+			Pattern pattern = Pattern.compile("https://hiskio.com/courses/(.*)");
+			Matcher matcher = pattern.matcher(url);
+			if (matcher.find()) {
+				String courseid = (matcher.group(1));
+				if (courseid.contains("/")) {
+					courseid = courseid.split("/")[0];
+				}
+				url = String.format("https://api.hiskio.com/v2/courses/%s", courseid) ;
+			}
+		}
+		
+		return url;
+	}
+	
+	protected WebTree AddChild() throws IOException{
+		System.out.println("---------------");
+		//Hahow
+		
+		String url = tree.root.webPage.url;
+		// When deal with Hahow and Hiskio url, normal request would only retrieve javascript
+		// Must convert to api url first
+		url = URLConverter(url);
+		System.out.println("Converted URL" + url);
+
+		try {
+			fetchContent(url);
+		}
+		catch (IOException e) {
+			System.out.println("Fail");
+			return tree;
+		}
+
+		
+		if (content == "") {
+			return tree;
+		}
+		
+		Document doc = Jsoup.parse(content);
+		Elements list =  doc.select("a");
+		ArrayList<WebPage> temp = new ArrayList<WebPage>();
+		System.out.println("Parent webpage"+url);
+		for (Element li : list) {
+			String citeUrl = li.attr("href");
+			String title = li.text();
+			//System.out.println(citeUrl);
+			// if either title or citeurl is empty, ignore this subwebsite
+			if(title.equals("") || citeUrl.equals("")) 
+			
+			{
+				continue;
+			}
+			
+			if (citeUrl.startsWith("/")) {
+				// Reconstruct the url if only path is presented
+				try {
+					URL uri = new URL(url);
+					citeUrl = uri.getProtocol() + "://" + uri.getHost() + citeUrl;
+				} 
+				catch (Exception e) {
+					// TODO Auto-generated catch block
+					continue;
+				}
+				
+			}
+			
+			if (!citeUrl.startsWith("http")) {
+				continue;
+			}
+			temp.add(new WebPage(citeUrl, title));
+			
+			count += 1;
+
+		}
+		
+		// sample at most three subpage from all a tags
+		
+		if (count >= 10) {
+
+			temp.subList((int)(0.2*count), (int)(0.8 * count));
+			Collections.shuffle(temp);
+		}
+		
+		else if (count >= 2) {
+			Collections.shuffle(temp);
+		}
+		
+		for (int i = 0 ; i < Math.min(count, 2); i++) {
+			
+			WebNode child  = new WebNode(temp.get(i));
+			System.out.println(temp.get(i).name + temp.get(i).url);
+			tree.root.addChild(child);
+		}
+		
+		
+		return tree;
+		
+	}
+}
